e16d170ac2013193575a4c66611c0c4faee41b84
Chad Lubrecht <chad@calubrecht.us>
2020-04-26 20:49:20 -0400

    More reliable and self contained DeleteWidget positioning.

--- src/MockDataService.tsx
+++ src/MockDataService.tsx
@@ -11,7 +11,7 @@ export class MockDataService extends DataService
     this.recipeBook = 
       new Map([
           ['Good Recipe',{name:'Good Recipe', text:'This is a recipe'}],
-          ['Better Recipe',{name:'Better Recipe', text:'This is a better recipe'}],
+          ['Better Recipe',{name:'Better Recipe', text:'This is a better recipe. It contains all the good things you love to eat and is full of sugar.', keyIngredients:['Sugar', 'Honey', 'Peanut Butter Cups']}],
           ['Awesomesauce Recipe',{name:'Awesomesauce Recipe', text:'This is the best recipe of all',
              keyIngredients:['Bacon', 'More Bacon'],
              commonIngredients:['Flour', 'Onions', 'Cheesy bits']}]
