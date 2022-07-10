4c2813f93b791dae48756fe54ea8ad227810e7ef
Chad Lubrecht <chad@calubrecht.us>
2022-07-10 15:12:04 -0400

    Typescript for tests

--- src/test/RecipeCard.test.tsx
+++ src/test/RecipeCard.test.tsx
@@ -1,7 +1,7 @@
 import  React from 'react';
 import renderer from 'react-test-renderer';
 import {RecipeCard} from '../RecipeCard';
-import {RecipeData} from './DataService';
+import {RecipeData} from '../DataService';
 
 interface WrapperProps
 {
